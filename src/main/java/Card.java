80f40c7d135936cebb2d67f1bc014c73686e4d50
Nathan Lin <nlin@athenian.org>
2022-01-04 08:03:39 -0800

    Update 2022

--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -1,92 +1,68 @@
-/**
- * Card.java
- *
- * Card represents a playing card.
- */
+// The Card class represents a single card in our card game
 public class Card {
 
-	/**
-	 * String value that holds the suit of the card
-	 */
-	private String suit;
-
-	/**
-	 * String value that holds the rank of the card
-	 */
+	// String value that holds the rank of the card
 	private String rank;
 
-	/**
-	 * int value that holds the point value.
-	 */
+	// String value that holds the suit of the card
+	private String suit;
+
+	// int value that holds the point value
 	private int pointValue;
 
-
-   /**
-	 * Creates a new Card instance.
-	 *
-	 * @param cardRank  a String value
-	 *                  containing the rank of the card
-	 * @param cardSuit  a String value
-	 *                  containing the suit of the card
-	 * @param cardPointValue an int value
-	 *                  containing the point value of the card
-	 */
+	// Constructor: Creates a new Card instance.
 	public Card(String cardRank, String cardSuit, int cardPointValue) {
-        // YOUR CODE HERE
-	}
-
-
-	/**
-	 * Accesses this Card's suit.
-	 * @return this Card's suit.
-	 */
-	public String getSuit() {
-        // YOUR CODE HERE
-        return "";
-   }
-
-	/**
-	 * Accesses this Card's rank.
-	 * @return this Card's rank.
-	 */
-	public String getRank() {
-        // YOUR CODE HERE
-        return "";
-	}
-
-   /**
-	 * Accesses this Card's point value.
-	 * @return this Card's point value.
-	 */
-	public int getPointValue() {
-        // YOUR CODE HERE
-        return -1;
-	}
-
-	/** Compare this card with the argument.
-	 * @param otherCard the other card to compare to this
-	 * @return true if the rank, suit, and point value of this card
-	 *              are equal to those of the argument;
-	 *         false otherwise.
-	 */
-	public boolean equals(Card otherCard) {
 		// YOUR CODE HERE
-        return false;
 	}
 
-	/**
-	 * Converts the rank, suit, and point value into a string in the format
-	 *     "[Rank] of [Suit] (point value = [PointValue])".
-	 * This provides a useful way of printing the contents
-	 * of a Deck in an easily readable format or performing
-	 * other similar functions.
-	 *
-	 * @return a String containing the rank, suit,
-	 *         and point value of the card.
-	 */
-	@Override
+	// Accesses this Card's rank.
+	public String getRank() {
+		// YOUR CODE HERE
+		return "";
+	}
+
+	// Accesses this Card's suit.
+	public String getSuit() {
+		// YOUR CODE HERE
+		return "";
+	}
+
+	// Accesses this Card's point value.
+	public int getPointValue() {
+		// YOUR CODE HERE
+		return -1;
+	}
+
+	// Compare this card with another
+	public boolean equals(Card other) {
+		// YOUR CODE HERE
+		return false;
+	}
+
+	// Converts the rank, suit, and point value into a string in the format
+	//     "[Rank] of [Suit] (point value = [PointValue])".
+	//     e.g. "Queen of Spades (point value = 10);
 	public String toString() {
-        // YOUR CODE HERE
-        return "";
+		// YOUR CODE HERE
+		return "";
+	}
+
+
+
+
+
+
+	// CODE BELOW HERE IS ALREADY WRITTEN:
+	// This version of equals is used in the testing code
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		else if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
+		else {
+			return this.equals((Card) o);
+		}
 	}
 }
--- src/main/java/Tester.java
+++
@@ -1,9 +0,0 @@
-/**
- * This is a class that tests the Card and Deck classees.
- */
-public class Tester {
-
-	public static void main(String[] args) {
-		// YOUR TESTING CODE HERE
-	}
-}
